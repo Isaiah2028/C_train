4bdff14d724eb3e1ef9672348340995d309665b3
Isaiah Lu <83460938+Isaiah2028@users.noreply.github.com>
2021-05-05 18:43:14 +0800

    变量作用域

---
+++ 01作用域/01作用域/作用域.c
@@ -0,0 +1,23 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+//变量的作用域，
+void test01() {
+
+
+
+}
+
+int main() {
+
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
+
